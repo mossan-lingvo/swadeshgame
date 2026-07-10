45b38487892813c3728f366de32681c5c09fcc27
mossan-lingvo <wjn6k5d9yc@privaterelay.appleid.com>
2026-07-10 22:16:18 +0900

    words.js を更新

--- words.js
+++ words.js
@@ -1,25 +1,14 @@
 const WORDS = [
 
-{lang:"turkish", word:"merhaba", answer:"こんにちは"},
-{lang:"turkish", word:"güneş", answer:"太陽"},
-{lang:"turkish", word:"ay", answer:"月"},
-{lang:"turkish", word:"su", answer:"水"},
-{lang:"turkish", word:"ekmek", answer:"パン"},
-{lang:"turkish", word:"ev", answer:"家"},
-{lang:"turkish", word:"köpek", answer:"犬"},
-{lang:"turkish", word:"kedi", answer:"猫"},
-{lang:"turkish", word:"kitap", answer:"本"},
-{lang:"turkish", word:"okul", answer:"学校"},
-
-{lang:"arabic", word:"مرحبا", answer:"こんにちは"},
-{lang:"arabic", word:"شمس", answer:"太陽"},
-{lang:"arabic", word:"قمر", answer:"月"},
-{lang:"arabic", word:"ماء", answer:"水"},
-{lang:"arabic", word:"خبز", answer:"パン"},
-{lang:"arabic", word:"بيت", answer:"家"},
-{lang:"arabic", word:"كلب", answer:"犬"},
-{lang:"arabic", word:"قط", answer:"猫"},
-{lang:"arabic", word:"كتاب", answer:"本"},
-{lang:"arabic", word:"مدرسة", answer:"学校"}
+{ja:"こんにちは", tr:"merhaba", ar:"مرحبا"},
+{ja:"太陽", tr:"güneş", ar:"شمس"},
+{ja:"月", tr:"ay", ar:"قمر"},
+{ja:"水", tr:"su", ar:"ماء"},
+{ja:"パン", tr:"ekmek", ar:"خبز"},
+{ja:"家", tr:"ev", ar:"بيت"},
+{ja:"犬", tr:"köpek", ar:"كلب"},
+{ja:"猫", tr:"kedi", ar:"قط"},
+{ja:"本", tr:"kitap", ar:"كتاب"},
+{ja:"学校", tr:"okul", ar:"مدرسة"}
 
 ];
