095ef4b9d4ff6ac9172430e3e35a768a807c2d38
mossan-lingvo <wjn6k5d9yc@privaterelay.appleid.com>
2026-07-10 21:42:46 +0900

    words.js を作成

---
+++ words.js
@@ -0,0 +1,25 @@
+const WORDS = [
+
+{lang:"turkish", word:"merhaba", answer:"こんにちは"},
+{lang:"turkish", word:"güneş", answer:"太陽"},
+{lang:"turkish", word:"ay", answer:"月"},
+{lang:"turkish", word:"su", answer:"水"},
+{lang:"turkish", word:"ekmek", answer:"パン"},
+{lang:"turkish", word:"ev", answer:"家"},
+{lang:"turkish", word:"köpek", answer:"犬"},
+{lang:"turkish", word:"kedi", answer:"猫"},
+{lang:"turkish", word:"kitap", answer:"本"},
+{lang:"turkish", word:"okul", answer:"学校"},
+
+{lang:"arabic", word:"مرحبا", answer:"こんにちは"},
+{lang:"arabic", word:"شمس", answer:"太陽"},
+{lang:"arabic", word:"قمر", answer:"月"},
+{lang:"arabic", word:"ماء", answer:"水"},
+{lang:"arabic", word:"خبز", answer:"パン"},
+{lang:"arabic", word:"بيت", answer:"家"},
+{lang:"arabic", word:"كلب", answer:"犬"},
+{lang:"arabic", word:"قط", answer:"猫"},
+{lang:"arabic", word:"كتاب", answer:"本"},
+{lang:"arabic", word:"مدرسة", answer:"学校"}
+
+];
